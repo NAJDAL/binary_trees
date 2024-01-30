b9cf427b2ef1a81d382f6782a576bc20edf291e8
NAJD ALEID <najdaleid6@gmail.com>
2024-01-30 03:08:21 -0800

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,10 +7,18 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree != NULL)
-    {
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
-        free(tree);
-    }
+	if (tree == NULL)
+	{
+		return;
+	}
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+
 }
