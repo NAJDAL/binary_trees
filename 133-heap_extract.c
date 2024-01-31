37f6a39b80c2270767147e626e65ddbf01f0b71d
NAJD ALEID <najdaleid6@gmail.com>
2024-01-31 06:30:25 -0800

    up

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - Extracts the root node of a Max Binary Heap.
+ *
+ * @root: A double pointer to the root node of the heap.
+ *
+ * Return: The value stored in the root node, or 0 on failure.
+ */
+int heap_extract(heap_t **root)
+{
+
+}
